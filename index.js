b24a1de8e4f856395cab63c09ad66dff73c09781
Sai Kiran <152156404+Sai94Kiran@users.noreply.github.com>
2024-07-25 23:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); // Middleware to parse JSON bodies.
 
 let items = [];
 
